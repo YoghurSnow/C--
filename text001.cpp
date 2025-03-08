4cb50764d327706e043f163c562e3162fde49918
YoghurSnow <139123453+YoghurSnow@users.noreply.github.com>
2025-03-08 18:10:20 +0800

    main

--- text001.cpp
+++ text001.cpp
@@ -1,16 +1,40 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
+#include<vector>
 
 using namespace std;
 
 int main(){
-	int sum=0;
-	for(int i=1;i<2021;i++){
-		string s = to_string(i);
-		sum += s.count('2');
+	//1.创建
+	vector<int> arr;
+	int a;
+	//2.添加
+	for(int i=0;i<9;i++){
+		cin>>a;
+		arr.push_back(a);
 	}
-	cout<<sum;
+	//3.输出
+	for(int num:arr){
+		cout<<num<<" ";
+	}
+	//4.访问某一个
+	cout<<endl<<"arr[2]="<<arr[2]<<endl;
+	//5.修改某一个
+	arr[3]=44;
+	//6.删除某一个
+	vector<int>::iterator it = arr.begin();
+	it += 5;
+	arr.erase(it);
+	//3.输出
+	for(int num:arr){
+		cout<<num<<" ";
+	}
+//0 1 2 3  4 5 6 7 8
+//1 2 3 4  5 6 7 8 9
+//1 2 3 44 5 7 8 9
+
+
 
 
 	return 0;
