2cf08eaf0ff54f6f55051410308255151ed5112e
YoghurSnow <139123453+YoghurSnow@users.noreply.github.com>
2025-03-12 21:16:06 +0800

    main

--- pta_moni_1/09.cpp
+++ pta_moni_1/09.cpp
@@ -3,45 +3,56 @@
 #include<vector>
 using namespace std;
 
-int xn,yn;
+int x0;
+int y0_;
 char arr[100][100];
+char sxzy[4]={'3','3','3','3'};
+int xx,yy;
 
-/*void dsp(int x,int y){
-    while(1){
-        //右
-        x++;
-        if(arr[x][y]!='0' || x>xn)x--;
-        //xia
-        y--;
-        if(arr[x][y]!='0' || y<yn)y++;
-        //zuo
-        x--;
-        if(arr[x][y]!='0' || x<0)x++;
-        //shang
-        y++;
-        if(arr[x][y]!='0' || y>0)y--;
-        
-        if(arr[x][y]=='T')break;
+bool dfs(int x,int y){
+    int dx[4]={0,0,-1,1};
+    int dy[4]={1,-1,0,0};
+
+    for(int i=0;i<4;i++){
+        xx=x+dx[i];
+        yy=y+dy[i];
+
+        if(xx<0 || xx>=x0 || yy>0 || yy<=y0_)continue;
+        sxzy[i]=arr[yy][xx];
+
+        if(arr[yy][xx]=='0'){
+            arr[yy][xx]='2' ;
+            dfs(xx,yy);
+        }
     }
-}*/
+    for(int j=0;j<4;j++){
+        if(sxzy[j] == '2'){
+            arr[yy][xx] = '4';
+            xx=x+dx[j];
+            yy=y+dy[j];
+            dfs(xx,yy);
+        }
+    }
+    if(arr[yy][xx]=='T')return true;
+    return false;
+
+}
+
 
 int main(){
-   cin>>xn>>yn;
+    cin>>x0>>y0_;
 
-   for(int i=0;i<yn;i++){
-        for(int j=0;j<xn;j++){
+    for(int i=0;i<y0_;i++){
+        for(int j=0;j<x0;j++){
             cin>>arr[i][j];
         }
-   }
-   cout<<"My God!";
-   //int x0=0,y0=0;
-   //dsp(x0,y0);
-   //上
-   
- 
-   //下
-   //左
-   //右
+    }
+    if(dfs(0,0)){
+        cout<<"!";
+    }else{
+        cout<<"?";
+    }
+    
 
     return 0;
 }
