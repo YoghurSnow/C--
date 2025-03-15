a2806291eb25a8a58dc45ea577a76c2827b3b382
YoghurSnow <139123453+YoghurSnow@users.noreply.github.com>
2025-03-15 20:21:06 +0800

    main

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,11 +1,55 @@
 #include<bits/stdc++.h>
-#include<algorithm>
-#include<vector>
 using namespace std;
 
+
 int main(){
-   
+	int n;
+	cin>>n;
+	
+	int arr[1000];
+	int k;
+	int t=0;
+	for(int i=0;i<n;i++){
+		cin>>k;
+		for(int j=0;j<k;j++){
+			cin>>arr[t];t++;
+		}
+	}
+	
+	int m;
+	cin>>m;
+	int brr[10000];
+	for(int i=0;i<m;i++){
+		cin>>brr[i];
+		for(int j=0;j<=t;j++){
+			if(brr[i]==arr[j]){
+				brr[i]=0;
+				break;
+			}
+		}
 
+	}
+	
+	for(int i=0;i<m;i++){
+		cout<<brr[i]<<" ";
+	}cout<<endl;
+	
+	int flag;
+	for(int i=0;i<m;i++){
+		if(brr[i]==0)continue;
+		flag=0;
+		for(int j=0;i<m;j++){
+			if(brr[j]==0)continue;
+			if(i==j)continue;
+			
+			if(brr[i]==brr[j])flag=1;
+			
+		}
+		if(flag==0)cout<<brr[i]<<" ";
+	}
 
-    return 0;
+	
+	return 0;
 }
+
+
